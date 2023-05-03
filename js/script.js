13f00a4df9c8ff548f1ae351f41b33d9a7973ede
Konstantin <konstantin.v737@gmail.com>
2023-05-04 01:17:44 +0300

    complete block download

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var swiper = new Swiper(".about-us__swiper", {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      effect: "fade",
+    });
